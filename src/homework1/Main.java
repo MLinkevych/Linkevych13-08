e738a807d7e3f4216477b9d7ee7f2655c4c226ec
Maksym Linkevych <95864524+MLinkevych@users.noreply.github.com>
2022-08-13 21:41:44 +0300

    first commit

--- src/homework1/Main.java
+++ src/homework1/Main.java
@@ -1,7 +1,10 @@
 package homework1;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-
+        ArrayList myCollection = new ArrayList(10);
     }
 }
